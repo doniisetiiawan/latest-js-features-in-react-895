2f493b3345f84f89fcdbdd9ce7677f4586e503f0
Doni Setiawan <donisetiawan.050@gmail.com>
2018-12-27 14:40:27 +0700

    Refactor variable

--- variables.js
+++ variables.js
@@ -1,8 +1,5 @@
-// can declare a global variable
 var name = "Carlos Santana";
-
-// cannot be a global
 let age = 30;
 
-console.log(window.name); // Carlos Santana
-console.log(window.age); // undefined
+console.log(name);
+console.log(age);
